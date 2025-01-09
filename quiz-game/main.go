54cf85811dbf7ded1694eda3b846b95be6d350ea
devdks <david.sovitrat@gmail.com>
2025-01-09 22:14:43 +0100

    Enhance quiz game with command-line flags for file input, time limit, and shuffling options by using goroutines and channels

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
-type Problem struct {
-	Question string
-	Answer   string
-}
+const usage = `
+Usage of quiz-game:
+	-f, --filename 	(string) A csv file in the format of 'question,answer' (default "problems.csv")
+	-l, --limit 	(int)	 The time limit for the quiz in seconds (default is 5 seconds)
+	-s ,--shuffle 	(bool) 	 Shuffle the questions
+`
 
 func main() {
-	var score, count, limit int
+	var score, limit int
 	var csvFileName string
+	var shuffle bool
+	timer := time.NewTimer(time.Duration(limit) * time.Second)
+	timer.Stop()
+	ansCh := make(chan string)
 
 	// Set up flags
-	flag.IntVar(&limit, "limit", -1, "The time limit for the quiz in seconds (default is 10 seconds)")
-	flag.StringVar(&csvFileName, " filename", "problems.csv", "A csv file in the format of 'question,answer'")
+	flag.IntVar(&limit, "limit", 5, "The time limit for the quiz in seconds (default is 5 seconds)")
+	flag.IntVar(&limit, "l", 5, "The time limit for the quiz in seconds (default is 5 seconds)")
+	flag.StringVar(&csvFileName, "filename", "problems.csv", "A csv file in the format of 'question,answer'")
 	flag.StringVar(&csvFileName, "f", "problems.csv", "A csv file in the format of 'question,answer'")
+	flag.BoolVar(&shuffle, "shuffle", false, "Shuffle the questions")
+	flag.BoolVar(&shuffle, "s", false, "Shuffle the questions")
+	flag.Usage = func() {
+		fmt.Print(usage)
+	}
 	// Parse the flags
 	flag.Parse()
 
@@ -34,31 +48,50 @@ func main() {
 
 	// Create a new csv reader
 	csv := csv.NewReader(file)
+	records, err := csv.ReadAll()
+	if err != nil {
+		panic(err)
+	}
 
-	// Loop through the csv file
-	for {
-		// Read the csv file
-		record, err := csv.Read()
-		if err == io.EOF {
-			break
+	if shuffle {
+		// Shuffle the records
+		rand.Shuffle(len(records), func(i, j int) {
+			records[i], records[j] = records[j], records[i]
+		})
+	}
+
+	// Loop through the records
+	for idx, record := range records {
+
+		question := record[0]
+		answer := strings.TrimSpace(record[1])
+
+		fmt.Println("--------------------")
+		fmt.Printf("Problem #%d: %s = ", idx+1, question)
+
+		go func() {
+			timer.Reset(time.Duration(limit) * time.Second)
+
+			var ans string
+			ans, err := bufio.NewReader(os.Stdin).ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+			ansCh <- strings.TrimSpace(ans)
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's up")
+			continue
+		case ans := <-ansCh:
+			// fmt.Printf("%s, %s\n", ans, answer)
+			if strings.EqualFold(ans, answer) {
+				score++
+			}
 		}
-		if err != nil {
-			panic(err)
-		}
-
-		count++
-
-		var problem Problem
-		problem.Question = record[0]
-		problem.Answer = strings.TrimSpace(record[1])
-
-		fmt.Printf("Problem #%d: %s = ", count, problem.Question)
-		var answer string
-		fmt.Scanln(&answer)
-		if answer == problem.Answer {
-			score++
-		}
+		fmt.Println("--------------------")
 
 	}
-	fmt.Printf("You have scored %d out of %d.", score, count)
+	fmt.Printf("✨✨ You have scored %d out of %d. ✨✨", score, len(records))
 }
